165e4d1c6fc3bc3f43277a6853918b0c9e967ce9
Praveen <learninjavadotcom@gmail.com>
2020-05-03 13:56:57 -0500

    Commit before build file change

--- react-widget-pack-app/src/App.js
+++ react-widget-pack-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
-import CommentContainer from "react-widget-pack";
+import { ReactWidgetPack } from 'react-widget-pack';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p />
-        <CommentContainer id={1} enable={true} />
+        <ReactWidgetPack id={1} enable={true} />
       </header>
     </div>
   );
